db7fabb704491cf32567690ccabc3ddf4c63552b
Ben <89508118+TDRework@users.noreply.github.com>
2023-03-13 23:18:17 -0400

    Use effectiveStrainTime for flow aim

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
@@ -62,7 +62,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             double travelDistance = osuPrevObj?.TravelDistance ?? 0;
             double distance = Math.Min(single_spacing_threshold, travelDistance + osuCurrObj.MinimumJumpDistance);
 
-            return speedBonus * doubletapness / effectiveStrainTime + (speedBonus * Math.Pow(distance / single_spacing_threshold, 3.5)) * doubletapness / strainTime;
+            return (speedBonus + speedBonus * Math.Pow(distance / single_spacing_threshold, 3.5)) * doubletapness / effectiveStrainTime;
         }
     }
 }
